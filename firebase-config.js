482c6852187da9b02d3737c568bf3cd7e47d1bce
kovaxacademy-cell <kovaxacademy@gmail.com>
2026-05-21 14:12:10 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,3 @@
+// Firebase/Supabase Auth placeholder pou Gmail/iCloud login reyèl.
+// Pa mete kle prive isit. Itilize Firebase public config sèlman oswa Supabase anon key.
+// Lè ou pare, mwen ka konekte Google Login ak Apple Login pou ou.
